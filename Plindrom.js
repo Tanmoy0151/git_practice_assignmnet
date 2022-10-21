d71101e48d0d164a2521ec9deb5d37d18f882858
Tanmoy0151 <royanupama782@gmail.com>
2022-10-21 08:30:02 +0530

    Prime number problem

--- Plindrom.js
+++
@@ -1,8 +0,0 @@
-let sum=0;
-let num=2021;
-let rec=num;
-while (num<1){
-let r=num%10;
-num=num/10;}
-if(sum=rec)console.loge{"This Is a Palinderom"}
-else {"Is not a Palindriom"}
---
+++ prime_number.js
@@ -0,0 +1,8 @@
+let num=9;
+count=0;
+for(i=0;i<=num;i++){if(num%i==0){
+    count++;
+}
+if (count>2){"this njot ashdhig "};// currect 
+else{}//put your code
+}
